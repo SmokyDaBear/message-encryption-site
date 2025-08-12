e3e66f3d609841fbd64da8b2c1fec739cef589bd
SmokyDaBear <jeshurungreen@gmail.com>
2025-08-12 18:35:26 -0500

    Live updates for encoding

--- scripts/encoder.js
+++ scripts/encoder.js
@@ -1,42 +1,3 @@
-// function rot13(message, key) {
-//   const cipher = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split(
-//     ""
-//   );
-//   const rotate = (char) => {
-//     let isCap = false;
-//     let newIndex;
-//     if (char.match(/[A-Z]/)) {
-//       isCap = true;
-//       char = char.toLowerCase();
-//     } else if (!char.match(/[a-z]/)) {
-//       return char;
-//     }
-//     newIndex = cipher.indexOf(char) + key;
-//     if (newIndex > 25) {
-//       newIndex -= 26;
-//     }
-//     char = cipher[newIndex];
-//     return isCap ? char.toUpperCase() : char;
-//   };
-//   const encode = (string) => {
-//     return string
-//       .split("")
-//       .map((item) => rotate(item))
-//       .join("");
-//   };
-//   return message
-//     .split(" ")
-//     .map((item) => encode(item))
-//     .join(" ");
-// }
-// const secretMessage = "Jgnnq, ncwpej vjg pwmgu";
-// const decode = (secretMessage) => {
-//   for (let i = 0; i < 26; i++) {
-//     console.log(i + rot13(secretMessage, i));
-//   }
-// };
-// decode(secretMessage);
-// decode(msg);
 const translate = (cipher, i, shift) => {
   const keyLength = cipher.length - 1;
   let replacement = i + shift;
@@ -64,14 +25,7 @@ function encode(
 ) {
   let cipher = key.split("");
 
-  let codex = getCodex(cipher, shift);
-  const logger = {};
-  let i = 0;
-  for (let [key, value] of Object.entries(codex)) {
-    logger[i] = { before: key, after: value };
-    i++;
-  }
-  console.table(logger);
+  let codex = getCodex(cipher, parseInt(shift));
 
   return string
     .split("")
@@ -80,19 +34,17 @@ function encode(
 }
 //railfence cipher
 function encodeRailFenceCipher(string, numberRails, decodeMode = false) {
-  // code
   const inputArr = string.split("");
   let answer = [];
   let railsLeft = +numberRails;
-  //create the rails of the answer as a 2d array;
   while (railsLeft) {
     answer.push([]);
     railsLeft--;
   }
   let cur = 0;
   let goingDown = true;
-  for (let i = 0; i < inputArr.length; i++) {
-    answer[cur].push(inputArr[i]);
+  for (let item of inputArr) {
+    answer[cur].push(item);
     if (goingDown) {
       cur++;
       if (cur >= numberRails - 1) {
@@ -109,7 +61,6 @@ function encodeRailFenceCipher(string, numberRails, decodeMode = false) {
 }
 
 function decodeRailFenceCipher(string, numberRails) {
-  // code
   let rowLengths = encodeRailFenceCipher(string, numberRails, true);
   let splitString = [];
   let lastChar = 0;
--- scripts/eventHandler.js
+++ scripts/eventHandler.js
@@ -1,35 +1,29 @@
-const inputMessageBox = document.querySelector("#input-message");
-const railsBox = document.querySelector("#num-rails");
-const rotationsBox = document.querySelector("#num-rotations");
-const keyBox = document.querySelector("#encryption-key");
-const outputMessageBox = document.querySelector("#output-message");
-const encodeBtn = document.querySelector("#encode-btn");
-const decodeBtn = document.querySelector("#decode-btn");
+const inputMessageBox = document.getElementById("input-message");
+const railsBox = document.getElementById("num-rails");
+const rotationsBox = document.getElementById("num-rotations");
+const keyBox = document.getElementById("encryption-key");
+const outputMessageBox = document.getElementById("output-message");
+const encodeBtn = document.getElementById("encode-btn");
+const decodeBtn = document.getElementById("decode-btn");
 let inputMessage;
-let encryptionKey =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{};:' ,<.>";
+let encryptionKey;
 let numRails = 3;
 let numRotations = 13;
 let outputMessage;
-
-encodeBtn.addEventListener("click", () => {
-  let message = inputMessage.value;
-  console.log(message);
-});
+let encryptionMode = "encrypt";
+const altKey =
+  "AbaBcCdDEeFfgGHhiIJjkKlLmMnN!@321#4$5%6^7&8*9(0)oOpPqQRrsStTuUvVwWxXyYzZ,<.>?/`~";
 
 inputMessageBox.addEventListener("keyup", (e) => {
-  inputMessage = e.target.value;
+  updateOutput(encryptionMode);
 });
+inputMessageBox.addEventListener("click", (e) => updateOutput(encryptionMode));
 
-railsBox.addEventListener("keyup", (e) => {
-  numRails = e.target.value;
-});
-rotationsBox.addEventListener("keyup", (e) => {
-  numRotations = e.target.value;
-});
-keyBox.addEventListener("keyup", (e) => {
-  encryptionKey = e.target.value;
-});
+railsBox.addEventListener("keyup", (e) => updateOutput(encryptionMode));
+railsBox.addEventListener("click", (e) => updateOutput(encryptionMode));
+rotationsBox.addEventListener("keyup", (e) => updateOutput(encryptionMode));
+rotationsBox.addEventListener("click", (e) => updateOutput(encryptionMode));
+keyBox.addEventListener("keyup", (e) => updateOutput(encryptionMode));
 const listKeys = (key, numRails, numRotations) => {
   return `<div class="flex-column align-start">
           <p>Key: ${key}</p>
@@ -38,23 +32,48 @@ const listKeys = (key, numRails, numRotations) => {
         </div>`;
 };
 
-encodeBtn.addEventListener("click", () => {
-  outputMessage = strongEncryption(
-    inputMessage,
-    numRails,
-    encryptionKey,
-    numRotations
-  );
+function updateOutput(encryptionMode) {
+  inputMessage = inputMessageBox.value;
+  numRails = railsBox.value;
+  if (!numRails) {
+    numRails = 3;
+  }
+  encryptionKey = keyBox.value;
+  if (!encryptionKey) {
+    encryptionKey =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{};:' ,.>";
+  }
+  numRotations = rotationsBox.value;
+  if (!numRotations) {
+    numRotations = 13;
+  }
+
+  if (encryptionMode == "encrypt") {
+    outputMessage = strongEncryption(
+      inputMessage,
+      numRails,
+      encryptionKey,
+      numRotations
+    );
+  } else if (encryptionMode == "decrypt") {
+    outputMessage = strongDecryption(
+      inputMessage,
+      numRails,
+      encryptionKey,
+      numRotations
+    );
+  } else {
+    alert("invalid encryption mode, select either 'encrypt' or 'decrypt'");
+  }
   outputMessage += listKeys(encryptionKey, numRails, numRotations);
   outputMessageBox.innerHTML = `<p>${outputMessage}</p>`;
+}
+
+encodeBtn.addEventListener("click", () => {
+  encryptionMode = "encrypt";
+  updateOutput(encryptionMode);
 });
 decodeBtn.addEventListener("click", () => {
-  outputMessage = strongDecryption(
-    inputMessage,
-    numRails,
-    encryptionKey,
-    numRotations
-  );
-  outputMessage += listKeys(encryptionKey, numRails, numRotations);
-  outputMessageBox.innerHTML = `<p>${outputMessage}</p>`;
+  encryptionMode = "decrypt";
+  updateOutput(encryptionMode);
 });
